07d2292dd39f5b379f52d5828256505612631d7b
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-05-30 04:32:36 +0300

    fix: deprecations

--- inc/class-plugin.php
+++ inc/class-plugin.php
@@ -19,7 +19,6 @@ final class Plugin {
 		register_activation_hook( $plugin, [ $this, 'activate' ] );
 		register_deactivation_hook( $plugin, [ $this, 'deactivate' ] );
 
-		add_action( 'plugins_loaded', [ $this, 'plugins_loaded' ] );
 		add_action( 'init', [ $this, 'init' ] );
 
 		if ( is_admin() ) {
@@ -41,11 +40,8 @@ final class Plugin {
 		flush_rewrite_rules();
 	}
 
-	public function plugins_loaded(): void {
-		load_plugin_textdomain( 'org-cpt', false, dirname( plugin_basename( __DIR__ ) ) . '/lang' );
-	}
-
 	public function init(): void {
+		load_plugin_textdomain( 'org-cpt', false, dirname( plugin_basename( __DIR__ ) ) . '/lang' );
 		add_action( 'rest_api_init', [ REST::class, 'get_instance' ] );
 
 		DocumentPostType::register();
